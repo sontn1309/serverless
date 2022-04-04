1936009f6da7379240bd107d287affb4519fca51
sontn1309 <51114397+sontn1309@users.noreply.github.com>
2022-04-04 22:18:31 +0700

    Update auth0Authorizer.ts

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-n68--8mq.us.auth0.com/.well-known/jwks.json';
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -57,11 +57,22 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const kid = jwt.header.kid
+  let certificate;
+  let signingAuthKey;
+  try {
+    const jwks = await Axios.get(jwksUrl)
+    signingAuthKey = jwks.data.keys.find(k => k.kid === kid)
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+    if (!signingAuthKey) {
+      throw new Error(`No signing key matching`);
+    }
+    certificate = `-----BEGIN CERTIFICATE-----\n${signingAuthKey.x5c[0]}\n-----END CERTIFICATE-----`;
+  } catch (e) {
+    logger.error('failed to retrieve certificate', { error: e.message })
+  }
+
+  return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
